2f2878761b14af76c8978417c489fb5de0cc1a48
PugalMuni <pugalmunir@pugalmunis-macbook-pro.local>
2019-02-26 23:12:22 +0530

    Initial Commit

---
+++ CustomTableView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CustomTableView
+//
+//  Created by PugalMuni on 26/02/19.
+//  Copyright © 2019 PugalMuni R. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
